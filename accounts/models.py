34107001f409e36fadbaf852c2e37729cd1fe2ea
“Francis <fgiles1@my.waketech.edu>
2026-09-14 22:28:04 -0400

    feat: create starting database models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class CustomerProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="customer_profile",
+    )
+    phone = models.CharField(max_length=20, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["user__last_name", "user__first_name"]
+
+    def __str__(self):
+        full_name = self.user.get_full_name()
+        return full_name or self.user.username
